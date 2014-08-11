e48f7eeb92b7c13ffdc20745783873721521a974
vietjtnguyen <viet@vietjtnguyen.com>
2014-08-11 16:18:25 -0700

    Renamed new to annotator.

--- front/js/annotator.js
+++ front/js/annotator.js
@@ -1,171 +1,147 @@
-var currentImage = new Image({
-  id: "2008_000003",
-  width: 500,
-  height: 333,
-  url: "../example-data/images/2008_000003.jpg",
-  comment: ""
-});
-var currentAnnotation = new ParallelLineAnnotation ({
-  image: currentImage
-});
-currentAnnotation.get("lines").add(new Line());
-var app = new App({
-  id: 0,
-  currentAnnotation: currentAnnotation
-});
+// Helpful links:
+// http://stackoverflow.com/questions/16265123/resize-svg-when-window-is-resized-in-d3-js
+// http://stackoverflow.com/questions/6942785/browsers-think-differently-about-window-innerwidth-and-document-documentelement
+// http://bl.ocks.org/mbostock/3892928
+// http://bl.ocks.org/mbostock/6123708
+// http://stackoverflow.com/questions/19075381/d3-mouse-events-click-dragend
+// http://getbootstrap.com/css
+// http://getbootstrap.com/components
+// http://backbonejs.org/#Model
+// http://backbonejs.org/#Collection
+// http://backbonejs.org/#FAQ-nested
+// http://stackoverflow.com/questions/18504235/understand-backbone-js-rest-calls
+// http://jstarrdewar.com/blog/2012/07/20/the-correct-way-to-override-concrete-backbone-methods/
+// https://github.com/mbostock/d3/wiki/Selections#animation--interaction
+//   The `this` context of an event callback in D3 is the DOM element.
+// http://stackoverflow.com/questions/19851171/nested-backbone-model-results-in-infinite-recursion-when-saving
+// http://stackoverflow.com/questions/6535948/nested-models-in-backbone-js-how-to-approach
+// http://stackoverflow.com/questions/1834642/best-practice-for-semicolon-after-every-function-in-javascript
+// http://www.erichynds.com/blog/backbone-and-inheritance
+// http://mjolnic.github.io/bootstrap-colorpicker/
+// http://api.jquery.com/css/
+// http://stackoverflow.com/questions/4633125/is-it-possible-to-get-all-arguments-of-a-function-as-single-object-inside-that-f
 
-var appView = new AppView({model: app, el: $("body")[0]});
+// This model represents the application state and doubly serves as the root
+// namespace for the application in the sense that point sets, views, and more
+// are attached to this model, but not as attributes.
+var AppState = Backbone.Model.extend({
 
-app.fetch({
-  update: true,
-  success: function(model, response, options) {
-    app.get("currentAnnotation").fetch({
-      success: function(model, response, options) {
-    });
-  },
-  error: function() { console.log("error"); }
-});
+  localStorage: new Backbone.LocalStorage("com.vietjtnguyen.annotator.AppState"),
 
-//Create the drag behavior that lets us drag points around.
-var drag = d3.behavior.drag()
-    .origin(function(d) { return d; })
-    .on("dragstart", dotDragStarted)
-    .on("drag", dotDragged)
-    .on("dragend", dotDragEnded);
-
-var lineSelection = d3.select("#origin").selectAll("polygon")
-  .data([{"width": 3, color: "#fff", opacity: 0.5, linecap: "round"},
-         {"width": 1, color: "#000", opacity: 1, linecap: "butt"}])
-  .enter()
-  .append("polygon")
-  .attr("fill", "none")
-  .attr("points", "")
-  .attr("vector-effect", "non-scaling-stroke")
-  .attr("stroke", function(d) { return d.color; })
-  .attr("stroke-width", function(d) { return d.width; })
-  .attr("stroke-linecap", function(d) { return d.linecap; })
-  .attr("stroke-linejoin", "round")
-  .style("opacity", function(d) { return d.opacity; });
-var dot = d3.select("#origin").append("g").attr("class", "dot");
-updateDots();
-
-function updateDots() {
-  // <http://bost.ocks.org/mike/circles/>
-  // <http://bost.ocks.org/mike/constancy/>
-  var dotsSelection = dot.selectAll("circle")
-    .data(app.get("currentAnnotation").get("lines").first().get("points"), function(d) { return d.id || d.cid; });
-  dotsSelection.transition()
-    .duration(500)
-    .attr("cx", function(d) { return d.x; })
-    .attr("cy", function(d) { return d.y; });
-  dotsSelection.enter().append("circle")
-    // http://stackoverflow.com/questions/10473328/how-to-draw-non-scalable-circle-in-svg-with-javascript 
-    .attr("vector-effect", "non-scaling-stroke")
-    .attr("r", 5)
-    .attr("cx", function(d) { return d.x; })
-    .attr("cy", function(d) { return d.y; })
-    .style({"opacity": 0,
-            "stroke-width": "1.5px",
-            stroke: "#000",
-            "stroke-opacity": 0.1,
-            fill: "#fff",
-            "fill-opacity": 0.1})
-    .on("click", dotClick)
-    .on("mouseover", function(d, i) {
-      d3.select(this).transition().duration(250)
-        .attr("r", 10)
-        .style("opacity", 1)
-        .style("fill-opacity", 0)
-        .style("stroke", "#f00")
-        .style("stroke-opacity", 1);
-    })
-    .on("mouseout", function(d, i) {
-      d3.select(this).transition().duration(250)
-        .attr("r", 5)
-        .style("opacity", 1)
-        .style("fill-opacity", 0.1)
-        .style("stroke", "#000")
-        .style("stroke-opacity", 0.1);
-    })
-    .call(drag)
-    .transition()
-    .duration(250)
-    .style("opacity", 1);
-  dotsSelection.exit()
-    .on("click", null)
-    .on("mouseover", null)
-    .on("mouseout", null)
-    .transition()
-    .duration(250)
-    .style("opacity", 0)
-    .remove();
-  updateLines();
-}
-
-function updateLines() {
-  lineSelection.attr("points", app.get("currentAnnotation").get("lines").first().toSvgCoord());
-}
-
-function dotDragStarted(d, i) {
-  d3.event.sourceEvent.stopPropagation();
-  d3.select(this).classed("dragging", true);
-}
-
-function dotDragged(d, i) {
-  d.x = d3.event.x;
-  d.y = d3.event.y;
-  d3.select(this).attr("cx", d.x).attr("cy", d.y);
-  updateLines();
-}
-
-function dotDragEnded(d, i) {
-  console.log("dotDragEnded");
-  console.log(d);
-  console.log(i);
-  d3.select(this).classed("dragging", false);
-  app.get("currentAnnotation").save();
-  updateLines();
-}
-
-function dotClick(d, i) {
-  console.log("dotClick");
-  console.log(d);
-  console.log(i);
-  // <http://stackoverflow.com/questions/19075381/d3-mouse-events-click-dragend> 
-  if (d3.event.defaultPrevented) return;
-  if (d3.event.ctrlKey) {
-    var points = app.get("currentAnnotation").get("lines").first().get("points");
-    points.remove(d);
-    updateDots();
-  }
-}
-
-// This function returns a two element array containing the "window" width and
-// height respectively.
-// <http://stackoverflow.com/questions/16265123/resize-svg-when-window-is-resized-in-d3-js>
-function getWindowSize() {
-    var w = window,
-        d = document,
-        e = d.documentElement,
-        g = d.getElementsByTagName('body')[0];
-    return [w.innerWidth || e.clientWidth || g.clientWidth,
-            w.innerHeight || e.clientHeight || g.clientHeight];
-}
-
-d3.select("#saveImageButton").on("click", function(d, i) {
-  app.get("currentAnnotation").get("lines").first().save();
-});
-
-d3.select("#openImageButton").on("click", function(d, i) {
-  app.get("currentAnnotation").get("lines").first().fetch({
-    success: function(model, response, options) {
-      console.log("fetch success");
-      console.log(model);
-      console.log(response);
-    },
-    error: function(model, response, options) {
-      console.log("fetch fail");
+  defaults: {
+    id: 0,
+    currentImage: null,
+    currentAnnotation: null,
+    currentSet: null,
+    background: "light",
+    grid: "off",
+    lineColor: "#000",
+    zoom: {
+      translate: [0, 0],
+      scale: 1
     }
-  });
-  updateDots();
+  },
+
+  initialize: function() {
+    var self = this;
+    self.on("change:lineColor", self.changeLineColor);
+  },
+
+  changeLineColor: function(model, color) {
+    var self = this;
+    self.pointSets.forEach(function(i) {
+      i.trigger("changeLineColor", color);
+    });
+  }
+
 });
+
+////////////////////////////////////////////////////////////////////////////////
+// Establish the application state and other models/views
+
+var appState = new AppState();
+
+// Attach the current image model to the application state but not as a model
+// attribute.
+appState.currentImage = new Image();
+
+// Attach the point set to the application state but not as a model attribute.
+appState.pointSets = new PointSetCollection({model: Line});
+
+// Create views
+appState.workingAreaView = new WorkingAreaView({model: appState, collection: appState.pointSets, el: $("body")[0]});
+appState.utilityBoxView = new UtilityBoxView({model: appState, el: $("#utilityBox")[0]});
+appState.listingView = new PointSetListView({collection: appState.pointSets, el: $("#lineControlSection")[0]});
+
+////////////////////////////////////////////////////////////////////////////////
+// Establish event hooks after all instances have been created
+
+// We want to the current image *model* to listen to changes in the
+// `currentImage` attribute on the application state so that the current image
+// model can update via a fetch accordingly.
+appState.currentImage.listenTo(appState, "change:currentImage", function() {
+  console.log("what");
+  appState.currentImage
+    .set("id", appState.get("currentImage"))
+    .fetch({
+      success: function() {
+        console.log("currentImage fetched");
+        console.log(appState.currentImage);
+      }
+    });
+});
+
+// Have the point set listen to the working area view for any "canvas clicks"
+// to know when to add points to the selected point set.
+appState.pointSets.listenTo(appState.workingAreaView, "workingAreaClick", function(mousePosition) {
+  console.log("pointSet workingAreaClick");
+  console.log(mousePosition);
+  var activePointSet = appState.pointSets.getSelected();
+  if (!activePointSet || activePointSet.isFull()) {
+    activePointSet = new appState.pointSets.model()
+    appState.pointSets.add(activePointSet);
+    appState.pointSets.select(activePointSet );
+  }
+  if (activePointSet) {
+    var point = new Point({
+      id: guid(),
+      x: mousePosition[0],
+      y: mousePosition[1]
+    });
+    var points = activePointSet.get("points");
+    activePointSet.set("points", _.union(points, [point]))
+    activePointSet.save();
+  }
+});
+
+////////////////////////////////////////////////////////////////////////////////
+// Fetch after all hooks have been established to return to previous state
+
+// Grab the application state if it was saved locally.
+appState.fetch({
+  success: function() {
+    console.log("AppState fetched.");
+  },
+  // If no application state was retrieved then initialize one.
+  error: function() {
+    console.log("AppState not fetched, initializing.");
+    appState.currentImage.set({
+      id: "2008_000003",
+      width: 500,
+      height: 333,
+      url: "../example-data/images/2008_000003.jpg",
+      comment: ""
+    }).save();
+    // Setting the current image on the application state will trigger the
+    // current image model to fetch from the server and update.
+    appState.set("currentImage", "2008_000003");
+    appState.save();
+  }
+});
+
+appState.pointSets.localStorage = new Backbone.LocalStorage("com.vietjtnguyen.annotator.Line"),
+appState.pointSets.fetch({
+  success: function() { console.log("points fetched"); },
+  error: function() { console.log("error fetching points"); }
+});
+
