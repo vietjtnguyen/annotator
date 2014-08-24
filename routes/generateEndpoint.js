e2d89fae7c8a9763a85798cc826eee4ebbaef992
vietjtnguyen <viet@vietjtnguyen.com>
2014-08-23 20:04:51 -0700

    Added comments.

--- routes/generateEndpoint.js
+++ routes/generateEndpoint.js
@@ -1,21 +1,81 @@
+// This module returns a function that can be used to generate/construct Express middleware that serves a REST-like API endpoint. The endpoint is defined using a hash object with the following keys:
+//
+// - `Model`: A [MongooseJS](http://mongoosejs.com/docs/models.html) model.
+// - `contextFields`: Model fields that are to be filled with constant values when a model is updated (POST, PUT). Specified as a hash object.
+// - `bodyFields`: An array of model fields that should be updated (POST, PUT) from the JSON body of a request. Note that if this is empty then no fields are drawn from the request body.
+// - `parentFields`: An array of model fields that should be updated (POST, PUT) or used in model query (GET, DELETE) from the request URL. The fields are defined with a hash object containing a `slug` and `field`. The `slug` refers to the name used in the Express URL (i.e. it is prepended with `:` when constructing the URL). The `field` refers to the actual model field that the URL param will be mapped to.
+// - `slug`: An intermediate URL segment name.
+// - `idField`: A hash object defining a URL to model field map, the same as the individual items in `parentFields`. The hash object must have the `slug` and `field` properties.
+//
+// The URL associated with an endpoint definition is constructed as follows:
+//
+// - `/:parentField1Slug/:parentField2Slug/slug
+//   - `GET`: Returns JSON array of all `Model`'s that match the parent fields.
+//   - `POST`: Creates a new entry in `Model` based on `contextFields`, `bodyFields`, and `parentFields`.
+// - `/:parentField1Slug/:parentField2Slug/slug/:idFieldSlug
+//   - `GET`: Returns an entry in `Model` found using `contextFields` and `idField`.
+//   - `PUT`: Updates an entry in `Model` based on `contextFields`, `bodyFields`, and `parentFields`. The entry is found using `contextFields` and `idField`.
+//   - `DELETE`: Deletes an entry in `Model` found using `contextFields` and `idField`.
+//
+// The following is an example endpoint definition:
+//
+// ```
+// example = {
+//   Model: PointSetModel,
+//   contextFields: {annotation: annotationName},
+//   bodyFields: ["description", "group", "points"],
+//   parentFields: [{slug: "image", field: "image"}],
+//   slug: "point-set",
+//   idField: {slug: "id", field: "_id"}
+// }
+// ```
+//
+// The example above results in the following endpoints:
+//
+// - `GET /:image/point-set`
+// - `POST /:image/point-set`
+// - `GET /:image/point-set/:id`
+// - `PUT /:image/point-set/:id`
+// - `DELETE /:image/point-set/:id`
+
+// Path is used for the `join` method to construct URLs. I'm not sure yet if
+// this is a good idea or not. At face value it seems fine.
 var path = require("path");
 
+// Underscore is used because it's so handy.
 var _ = require("underscore");
 
+// Express is needed since our generation function returns an Express router to
+// be used as middleware.
 var express = require("express");
 
+// The module exports a function that generates middleware based on an endpoint
+// definition.
 module.exports = function createEndpoint(endpoint) {
 
+  // Fill in any undefined endpoint definition fields.
+  // <http://underscorejs.org/#defaults>
+  endpoint = _.defaults(endpoint, {
+    contextFields: {},
+    bodyFields: [],
+    parentFields: [],
+    slug: "",
+    idField: {}
+  });
+
+  // Define the collection URL. This URL returns an array of entries on GET and
+  // creates a new entry on POST.
   var collectionUrl = "/";
   endpoint.parentFields.forEach(function(parentField) {
     collectionUrl = path.join(collectionUrl, ":" + parentField.slug);
   });
   collectionUrl = path.join(collectionUrl, endpoint.slug);
+
+  // Define the item URL. This URL returns, updates, and deletes a single entry
+  // on GET, PUT, and DELETE respectively.
   var itemUrl = path.join(collectionUrl, ":" + endpoint.idField.slug);
 
-  console.log(collectionUrl);
-  console.log(itemUrl);
-
+  // Create our Express router that will be our middleware.
   var router = express.Router();
 
   router.route(collectionUrl)
@@ -26,9 +86,6 @@ module.exports = function createEndpoint(endpoint) {
       endpoint.parentFields.forEach(function(parentField) {
         newModel[parentField.field] = request.params[parentField.slug];
       });
-      console.log(request.params);
-      console.log(request.body);
-      console.log(newModel);
       newModel.save(function(error) {
         if (error) {
           response.send(error);
@@ -41,9 +98,6 @@ module.exports = function createEndpoint(endpoint) {
       endpoint.parentFields.forEach(function(parentField) {
         query[parentField.field] = request.params[parentField.slug];
       });
-      console.log(request.params);
-      console.log(request.body);
-      console.log(query);
       endpoint.Model.find(query, function(error, models) {
         if (error) {
           response.send(error);
@@ -59,9 +113,6 @@ module.exports = function createEndpoint(endpoint) {
       endpoint.parentFields.forEach(function(parentField) {
         query[parentField.field] = request.params[parentField.slug];
       });
-      console.log(request.params);
-      console.log(request.body);
-      console.log(query);
       endpoint.Model.findOne(query, function(error, model) {
         if (error) {
           response.send(error);
@@ -75,16 +126,12 @@ module.exports = function createEndpoint(endpoint) {
       endpoint.parentFields.forEach(function(parentField) {
         query[parentField.field] = request.params[parentField.slug];
       });
-      console.log(request.params);
-      console.log(request.body);
-      console.log(query);
       endpoint.Model.findOne(query, function(error, model) {
         if (error) {
           response.send(error);
         }
         _.extend(model, endpoint.contextFields);
         _.extend(model, _.pick(request.body, endpoint.bodyFields));
-        console.log(model);
         endpoint.parentFields.forEach(function(parentField) {
           model[parentField.field] = request.params[parentField.slug];
         });
@@ -102,9 +149,6 @@ module.exports = function createEndpoint(endpoint) {
       endpoint.parentFields.forEach(function(parentField) {
         query[parentField.field] = request.params[parentField.slug];
       });
-      console.log(request.params);
-      console.log(request.body);
-      console.log(query);
       endpoint.Model.remove(query, function(error, model) {
         if (error) {
           response.send(error);
